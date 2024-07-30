9f0fea031d1a7764bc8df05754f23a4c81855bda
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-07-30 16:39:03 +0000

    Basic

---
+++ basics_js/test.js
@@ -0,0 +1,10 @@
+// ======== variables =============
+//   4 type ways declared variables
+//   ========== let  ===========
+
+{
+    let a = 10;
+    let b = 12;
+    let c = a + b;
+    console.log(c)
+}
