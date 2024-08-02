a0dc6d4e8c41a05a1f32a8fefaac4e542271ff80
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-08-02 05:57:12 +0000

    update

--- basics_js/test.js
+++ basics_js/test.js
@@ -1,359 +1,359 @@
 
-// //  // ======== variables =============
-// //  //   4 type ways declared variables
-// //  // automatically , let, const, var
-// //  //   ========== let block scope  ===========
+//  // ======== variables =============
+//  //   4 type ways declared variables
+//  // automatically , let, const, var
+//  //   ========== let block scope  ===========
 
-// {
-//     let a = 10;
-//     let b = 12;
-//     let c = a + b;
-//     console.log(c)
-// }
-// {
-//     let a = 15;
-//         a = 23;
-//         console.log(a)
-// }
+{
+    let a = 10;
+    let b = 12;
+    let c = a + b;
+    console.log(c)
+}
+{
+    let a = 15;
+        a = 23;
+        console.log(a)
+}
 
-// // //  ========== const block scope  ===========
+// //  ========== const block scope  ===========
 
-// {
-//     const a = 19;
-//     console.log(a)
-// }
+{
+    const a = 19;
+    console.log(a)
+}
 
-// // //  ========== var global scope  ===========
+// //  ========== var global scope  ===========
 
-// var a = 33;
-// var a = 44;
-// console.log(a)
-// {
-//     var a = 6;
-//     var a = 454;
-//     console.log(a)
-// }
+var a = 33;
+var a = 44;
+console.log(a)
+{
+    var a = 6;
+    var a = 454;
+    console.log(a)
+}
 
-// // // ======== operators ==========
-// // // airthmatic operators
+// // ======== operators ==========
+// // airthmatic operators
 
-// {
-//     let a = 55;
-//     let b = 3;
-//     console.log("a + b =", a+b)
-//     console.log("a - b =", a-b)
-//     console.log("a / b =", a/b)
-//     console.log("a * b =", a*b)
-//     console.log("a ** b =", a**b)
-//     console.log("a % b =", a%b)
-//     console.log("++a =", ++a)
-//     console.log("--a =", --a)
+{
+    let a = 55;
+    let b = 3;
+    console.log("a + b =", a+b)
+    console.log("a - b =", a-b)
+    console.log("a / b =", a/b)
+    console.log("a * b =", a*b)
+    console.log("a ** b =", a**b)
+    console.log("a % b =", a%b)
+    console.log("++a =", ++a)
+    console.log("--a =", --a)
     
-// }
+}
 
-// // //assignment operators
+// //assignment operators
 
-//  {
-// let a = 1;
-//  a += 5;
-//  console.log(a)
-//  let b = 1;
-//  b -= 5;
-//  console.log(b)
-//  let c = 1;
-//  c /= 5;
-//  console.log(c)
-//  let d = 1;
-//  d *= 5;
-//  console.log(d)
-//  let e = 1;
-//  e %= 5;
-//  console.log(e)
-//  let f = 1;
-//  f %= 5;
-//  console.log(f)
-//   }
+ {
+let a = 1;
+ a += 5;
+ console.log(a)
+ let b = 1;
+ b -= 5;
+ console.log(b)
+ let c = 1;
+ c /= 5;
+ console.log(c)
+ let d = 1;
+ d *= 5;
+ console.log(d)
+ let e = 1;
+ e %= 5;
+ console.log(e)
+ let f = 1;
+ f %= 5;
+ console.log(f)
+  }
 
-// //  // comparison operators
+//  // comparison operators
 
-//  {
-//     let comp1 = 30;
-//     let comp2 = 22;
-//     console.log("comp1 == comp2", comp1 == comp2)
-//     console.log("comp1 != comp2", comp1 != comp2)
-//     console.log("comp1 === comp2", comp1 === comp2)
-//     console.log("comp1 !== comp2", comp1 !== comp2)
-//     console.log("comp1 < comp2", comp1 < comp2)
-//     console.log("comp1 > comp2", comp1 > comp2)
-//     console.log("comp1 <= comp2", comp1 <= comp2)
-//     console.log("comp1 >= comp2", comp1 >= comp2)
-//  } 
+ {
+    let comp1 = 30;
+    let comp2 = 22;
+    console.log("comp1 == comp2", comp1 == comp2)
+    console.log("comp1 != comp2", comp1 != comp2)
+    console.log("comp1 === comp2", comp1 === comp2)
+    console.log("comp1 !== comp2", comp1 !== comp2)
+    console.log("comp1 < comp2", comp1 < comp2)
+    console.log("comp1 > comp2", comp1 > comp2)
+    console.log("comp1 <= comp2", comp1 <= comp2)
+    console.log("comp1 >= comp2", comp1 >= comp2)
+ } 
 
-// // //  ========== logical operators ==========
+// //  ========== logical operators ==========
 
-// {
-//     let x = 10;
-//     let y = 5;
-//     console.log(x>y && x==10)
-// }
-// {
-//     let x = 10;
-//     let y = 5;
-//     console.log(x>y || x==10)
-// }
-// {
-//     let x = 10;
-//     let y = 5;
-//     console.log(!x>y)
-// }
-
-
-// // // ============= ternary operator ===========
-
-// { let x = 10
-// console.log(x<9? "ok" :"not ok" ) }
-
-// // // ========= Datatype ==========
-// // // primitive datatype 7 type = NNBBSSU = null, number, boolean, bigint, string, symbol, undefined
-
-// {
-//     let a = null;
-//     let b = 123;
-//     let c = true; // false
-//     let d = BigInt (324) + BigInt(55)
-//     let e = "hello";
-//     let f = Symbol("Hello World")
-//     let g;
-//     console.log(a, b, c, d, e, f, g)
-//     console.log(d)
-//     console.log(typeof c)
-// }
+{
+    let x = 10;
+    let y = 5;
+    console.log(x>y && x==10)
+}
+{
+    let x = 10;
+    let y = 5;
+    console.log(x>y || x==10)
+}
+{
+    let x = 10;
+    let y = 5;
+    console.log(!x>y)
+}
 
 
-// // // non-primitive datatype = object = key value
+// // ============= ternary operator ===========
 
-// { 
-//     const item = {
-//         "Khem": true,
-//         "Anuj": false,
-//         "vicky": 44,
-//         "Ajay": null,
-//     }
-//     console.log(item["anuj"])
-// }
+{ let x = 10
+console.log(x<9? "ok" :"not ok" ) }
+
+// // ========= Datatype ==========
+// // primitive datatype 7 type = NNBBSSU = null, number, boolean, bigint, string, symbol, undefined
+
+{
+    let a = null;
+    let b = 123;
+    let c = true; // false
+    let d = BigInt (324) + BigInt(55)
+    let e = "hello";
+    let f = Symbol("Hello World")
+    let g;
+    console.log(a, b, c, d, e, f, g)
+    console.log(d)
+    console.log(typeof c)
+}
+
+
+// // non-primitive datatype = object = key value
+
+{ 
+    const item = {
+        "Khem": true,
+        "Anuj": false,
+        "vicky": 44,
+        "Ajay": null,
+    }
+    console.log(item["anuj"])
+}
 
 
 // // // ============= conditional statements ================
 // // //  if statement, if else statement, if else if else statement
 
 
-// {
-//     let x = "Hello"
-//  x = Number.parseInt(x) // converting the string to a number
-// console.log(typeof x)
-// }
+{
+    let x = "Hello"
+ x = Number.parseInt(x) // converting the string to a number
+console.log(typeof x)
+}
 
-// // // ======== if statement =========
-// {
-// let a = 25
-// if(a<30){
-//     console.log('color')
-//    }
-// }
+// // ======== if statement =========
+{
+let a = 25
+if(a<30){
+    console.log('color')
+   }
+ }
 
 
 // // // ======== if else statement =========
-// {
-// let a = 250
-// if(a<30){
-//     console.log('color')
+{
+let a = 250
+if(a<30){
+    console.log('color')
 
-// }else(
-//     console.log('black color')
-// )
-// }
+}else(
+    console.log('black color')
+)
+}
 
 // // // ======== if else if statement =========
 
-// {
+{
 
-// let balance = 100;
-// if (balance<500){
-//     console.log('less then 500')
-// } else if(balance<750)
-// {
-//     console.log('less then 750')
-// }else if(balance<950){
-//     console.log('less then 950')
-// }else{
-//     console.log('less then 1200')
-// }
+let balance = 100;
+if (balance<500){
+    console.log('less then 500')
+} else if(balance<750)
+{
+    console.log('less then 750')
+}else if(balance<950){
+    console.log('less then 950')
+}else{
+    console.log('less then 1200')
+}
 
-// }
+}
 
-//    {
-// let x = 20;
-// if (x>=18){
-//     console.log('you are adult')
-// } else{
-//     console.log('you are minor')
-// }
-//    }
-
-
-// {
-// let rating = 3.5
-// if (rating<=2){
-//     console.log('bad')
-// } else if(rating>=4){
-//     console.log('good')
-// }else{
-//     console.log('average')
-// }
-// }
-
-// {
-// let userLogIn = true;
-// let userLogOut = true;
-// let usercount = 4;
-// if(userLogIn && userLogOut && usercount){
-//     console.log('successfully')
-// }
-// }
-
-// {
-// let userLogIn = true;
-// let userLogOut = true;
-// let usercount = 4;
-// if(userLogIn || userLogOut || usercount){
-//     console.log('successfully')
-// }
-// }
+   {
+let x = 20;
+if (x>=18){
+    console.log('you are adult')
+} else{
+    console.log('you are minor')
+}
+   }
 
 
-// // // ============practice operators & conditions ============
+{
+let rating = 3.5
+if (rating<=2){
+    console.log('bad')
+} else if(rating>=4){
+    console.log('good')
+}else{
+    console.log('average')
+}
+}
 
-// // {
-// // let age = prompt('whta is your age')
-// // if (age<9 && age<20){
-// //     console.log('your age lies between 10 and 20')
-// // }
-// // else{
-// //     console.log('your age doesnt lies between 10 and 20')
-// // }
-// // }
+{
+let userLogIn = true;
+let userLogOut = true;
+let usercount = 4;
+if(userLogIn && userLogOut && usercount){
+    console.log('successfully')
+}
+}
+
+{
+let userLogIn = true;
+let userLogOut = true;
+let usercount = 4;
+if(userLogIn || userLogOut || usercount){
+    console.log('successfully')
+}
+}
+
+
+// // ============practice operators & conditions ============
 
 // {
-// let age = 13;
-// switch (age){
-//     case 10:
-//     console.log('your age is 10')
-//     break
-//     case 11:
-//     console.log('your age is 11')
-//     break
-//     case 12:
-//     console.log('your age is 12')
-//     break
-//     case 13:
-//     console.log('your age is 13')
-//     break
-//     default:
-//     console.log('your age is not special')
+// let age = prompt('whta is your age')
+// if (age<9 && age<20){
+//     console.log('your age lies between 10 and 20')
+// }
+// else{
+//     console.log('your age doesnt lies between 10 and 20')
 // }
 // }
 
-// // ============= loops =================
+{
+let age = 13;
+switch (age){
+    case 10:
+    console.log('your age is 10')
+    break
+    case 11:
+    console.log('your age is 11')
+    break
+    case 12:
+    console.log('your age is 12')
+    break
+    case 13:
+    console.log('your age is 13')
+    break
+    default:
+    console.log('your age is not special')
+}
+}
+
+// ============= loops =================
 
 // // ======= for loop =========
-// {
-// for (let i = 0 ; i < 20; i++){
-//     console.log(i)
-// }
-// }
+{
+for (let i = 0 ; i < 20; i++){
+    console.log(i)
+}
+}
 
-// {
-// let sum = 0;
-// let n = 6;
-// for (let i = 0 ; i <= n ; i++){
-//   sum = sum + i;
-// }
-// console.log('sum=', sum)
-// }
+{
+let sum = 0;
+let n = 6;
+for (let i = 0 ; i <= n ; i++){
+  sum = sum + i;
+}
+console.log('sum=', sum)
+}
 
-// {
-// let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat']
-// for (let i = 0; i < car.length; i++) {
-//        console.log(car[i]);
-//     }
-//    }
+{
+let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat']
+for (let i = 0; i < car.length; i++) {
+       console.log(car[i]);
+    }
+   }
 
 
 // // ============= while loop =============
 
-// {
-// let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat' , 'maruti']
+{
+let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat' , 'maruti']
 
-// let i = 0
-// while(i <= car.length){
-//  console.log(car[i])
-//  i++
-// }
-// }
+let i = 0
+while(i <= car.length){
+ console.log(car[i])
+ i++
+}
+}
 
 // //  ============== do while loop ==============
 
-// {
-// let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat' , 'maruti']
+{
+let car = ['Volvo', 'BMW', 'Scoda', 'Ford', 'Fiat' , 'maruti']
 
-// let i = 0
-// do{
-//    console.log(car[i])
-//    i++;
-// }while(i <= car.length);
-// }
+let i = 0
+do{
+   console.log(car[i])
+   i++;
+}while(i <= car.length);
+}
 
 // // ============== for-of loop val return kre ga =============
 
-// {
-// let str = "Khemsingh"
+{
+let str = "Khemsingh"
 
-// for(let i of str) // iterator
-//    {
-//    console.log('i=',i);
-// }
-// }
+for(let i of str) // iterator
+   {
+   console.log('i=',i);
+}
+}
 
 
 // ============== for-in loop key return kre ga   =============
 
-// {
-// let student ={
-//    Name : 'Khem singh',
-//    Class : 12,
-//    Age : 18,
-//    Marks : 33,
-//    isPass : true,
-// }
-// for(let key in student){
-//    con
-// }sole.log('key=',key,'value =',student[key])
-//  }
+{
+let student ={
+   Name : 'Khem singh',
+   Class : 12,
+   Age : 18,
+   Marks : 33,
+   isPass : true,
+}
+for(let key in student){
+   console.log('key=',key,'value =',student[key])
+}
+ }
 
 // //  ========== practice =========
-//  for ( num = 0 ; num <= 100 ; num++){
-//    if(num % 2 === 0 ){
-//          //even number 
-//          console.log(num)
-//       }
-//  }
+ for ( num = 0 ; num <= 100 ; num++){
+   if(num % 2 === 0 ){
+         //even number 
+         console.log(num)
+      }
+ }
 
-//  for ( num=0 ; num<=100 ; num++){
-//    if(num % 2 !== 0 ){
-//          //odd number 
-//          console.log(num)
-//       }
-//  }
+ for ( num=0 ; num<=100 ; num++){
+   if(num % 2 !== 0 ){
+         //odd number 
+         console.log(num)
+      }
+ }
 
 //  {
 //  let game = 33;
@@ -364,85 +364,86 @@
 //  }console.log('good');
 // }
 
-// {
-//  let myArray = ['Spidermen', 'thor', 'betmen']
-//  for(let key in myArray){
-//        console.log('key=',key,'value =',myArray[key])
-//     }
-//    }
+{
+ let myArray = ['Spidermen', 'thor', 'betmen']
+ for(let key in myArray){
+       console.log('key=',key,'value =',myArray[key])
+    }
+   }
 
-//    {
-//     let myArray = ['Spidermen', 'thor', 'betmen']
-//     for(let i = 0; i < myArray.length; i++){
-//       console.log(i,myArray[i])
-//     }
-//    }
+   {
+    let myArray = ['Spidermen', 'thor', 'betmen']
+    for(let i = 0; i < myArray.length; i++){
+      console.log(i,myArray[i])
+    }
+   }
  
-//    {
-//    for(let i=1; i<=20; i++){
+   {
+   for(let i=1; i<=20; i++){
       
-//       if(i == 11){
-//          console.log(`11 value dtected ${i}` )
-//         // break
-//          // continue
-//       }
-//       console.log(i)
-//    }
-// }
+      if(i == 11){
+         console.log(`11 value dtected ${i}` )
+        // break
+         // continue
+      }
+      console.log(i)
+   }
+}
  
 //    // ========== function ===========
-// {
-//    function myFunction(){
-//       console.log('Hello World!');
-//    }
-//    myFunction();
-// }
+{
+   function myFunction(){
+      console.log('Hello World!');
+   }
+   myFunction();
+}
 
 
-// {
-//    function myFunction(msg){
-//       //parameter input
-//       console.log(msg);
-//    }
-//    myFunction('Hello world');
-// }
+{
+   function myFunction(msg){
+      //parameter input
+      console.log(msg);
+   }
+   myFunction('Hello world');
+}
 
 
-// {
-//    function sum(x, y){
-//       console.log(x * y)
-//    }
-//    sum(53, 24)
-// }
+{
+   function sum(x, y){
+      console.log(x * y)
+   }
+   sum(53, 24)
+}
 
     
-//    {
-//    function mul(x, y){
-//         mul = x * y;
-//         return mul;
-//       }
-//       let val = mul(53, 24)
-//       console.log(val)
-//    }
+  {
+   function mul(x, y){
+        mul = x * y;
+        return mul;
+      }
+      let val = mul(53, 24)
+      console.log(val)
+   }
 
-
-//   let myArray=['44','54', '65', '54', '22']
-//     const newArr = myArray[2] * 2 ;
-//     console.log(newArr)
+{
+  let myArray=['44','54', '65', '54', '22']
+    const newArr = myArray[2] * 2 ;
+    console.log(newArr)
+   }
 
 // //   =============Arrow Function=============
 // // ======== sum function ========
 // // modern js
 
-// {
-//    let arrowSum=(a, b)=>{
-//    console.log(a + b);
-// };
-// arrowSum(4, 8)
-// }
+{
+   let arrowSum=(a, b)=>{
+   console.log(a + b);
+};
+arrowSum(4, 8)
+ }
 
-// // ======== multiplication function ========
-// // modern js
+// // // ======== multiplication function ========
+// // // modern js
 
 {
 let arrowMul=(a, b)=>{
