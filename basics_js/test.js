94f45511d669b0ed894d1bfe07b0287da8ca3d5f
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-08-05 09:44:32 +0000

    arrays

--- basics_js/test.js
+++ basics_js/test.js
@@ -466,221 +466,322 @@
 // ==========practice loops=========== 
 
 // ========== Question = #1 — Print the numbers from 0 to 15 =============
-{
-   for(let i = 0; i <= 15; i++){
-      console.log(i);
-   }
-}
+// {
+//    for(let i = 0; i <= 15; i++){
+//       console.log(i);
+//    }
+// }
 
-// ========== Question = #2 — Print the numbers from 12 to 24 =============
+// // ========== Question = #2 — Print the numbers from 12 to 24 =============
 
-{
-   for(let i = 12; i <= 24; i++){
-      console.log(i);
-   }
-}
+// {
+//    for(let i = 12; i <= 24; i++){
+//       console.log(i);
+//    }
+// }
 
-// ========== Question = #3 — Print the ODD numbers from 7 to 31 =============
+// // ========== Question = #3 — Print the ODD numbers from 7 to 31 =============
 
-{
-   for(let i = 7; i <= 31; i++){
-      if(i % 2 !=0){
-           console.log(i)
-      }
-   }
-}
+// {
+//    for(let i = 7; i <= 31; i++){
+//       if(i % 2 !=0){
+//            console.log(i)
+//       }
+//    }
+// }
 
-// ========== Question = #4 — Print the EVEN numbers from 10 to -20 =============
+// // ========== Question = #4 — Print the EVEN numbers from 10 to -20 =============
 
-{
-   for(let i = 10; i >= -20; i--){
-      if(i % 2 ==0){
-           console.log(i)
-      }
-   }
-}
+// {
+//    for(let i = 10; i >= -20; i--){
+//       if(i % 2 ==0){
+//            console.log(i)
+//       }
+//    }
+// }
 
-//  ========== Question = 5 #5 — Iterate through all numbers from 1 to 45. Print the following:
-                                 // For multiples of 3 print “Fizz”
-                                 // For multiples of 5 print “Buzz”
-                                 // For multiples of 3 and 5 print “FizzBuzz” =============
+// //  ========== Question = 5 #5 — Iterate through all numbers from 1 to 45. Print the following:
+//                                  // For multiples of 3 print “Fizz”
+//                                  // For multiples of 5 print “Buzz”
+//                                  // For multiples of 3 and 5 print “FizzBuzz” =============
 
-{
-   for(let i = 1; i <= 45; i++){
-  if(i % 3 == 0 && i % 5 == 0){
-   console.log("For 3,5 : FizzBuzz",i);
-  }else if(i % 5 ==0) {
-   console.log("For 5 : Buzz",i);
-  }else if(i % 3 == 0) {
-      console.log("For 3 : Fizz",i);
-  }
-   }
-}
-
-
-// ========== Question = #6 — Print all the elements of the following array =============
-
-{
-   const heros=["Spiderman", "Betman", "Thor", "Hulk", "Antman"]
-   for(let i = 0; i < heros.length - 1; i++){
-      console.log(heros[i])
-   }
-}
-
-// ========== Question = #7 — Calculate the sum of all the numbers in the following array =============
-
-{
-    let num = [1, 12, 22, 123, 43];
-  let sum = 0;
-  for(let i = 0; i < num.length; i++){
-     sum = sum + num[i];
-  }
-  //console.log('This is sum: ' , sum );
-  console.log(`this is sum ${sum}`)
-}
-
-// ==========practice If else=========== 
-
-// ========== Question = #1 — Determine If a Number Is Positive =============
-//Write a program that determines whether a given number is positive or not.
+// {
+//    for(let i = 1; i <= 45; i++){
+//   if(i % 3 == 0 && i % 5 == 0){
+//    console.log("For 3,5 : FizzBuzz",i);
+//   }else if(i % 5 ==0) {
+//    console.log("For 5 : Buzz",i);
+//   }else if(i % 3 == 0) {
+//       console.log("For 3 : Fizz",i);
+//   }
+//    }
+// }
 
 
-{
-   let a = 5;
-   if( a > 4){
-      console.log(" This Number is positive")
-   }else {
-      console.log("This number is not positive")
-   }
-}
+// // ========== Question = #6 — Print all the elements of the following array =============
 
-// ========== Question =  #2 — Check Even or Odd =============
-// Write a program that checks if a number is even or odd.
+// {
+//    const heros=["Spiderman", "Betman", "Thor", "Hulk", "Antman"]
+//    for(let i = 0; i < heros.length - 1; i++){
+//       console.log(heros[i])
+//    }
+// }
 
-{
-let number = 9;
-if ( number % 2 ===0){
-   console.log('this is even number')
-}
-else {
-   console.log('THis is odd number')
-}
-}
+// // ========== Question = #7 — Calculate the sum of all the numbers in the following array =============
 
+// {
+//     let num = [1, 12, 22, 123, 43];
+//   let sum = 0;
+//   for(let i = 0; i < num.length; i++){
+//      sum = sum + num[i];
+//   }
+//   //console.log('This is sum: ' , sum );
+//   console.log(`this is sum ${sum}`)
+// }
 
-// ========== Question =   #3 — Determine the Greater Number =============
-//Write a program to determine the greater of two numbers.
+// // ==========practice If else=========== 
 
-{
-   let x = 13;
-   let y = 17;
-   if(x > y){
-      console.log('x is greater')
-   }else{
-      console.log('y is greater')
-   }
-}
-
-// ========== Question =    #4 — Grade Calculator ============
-//Write a program that assigns a letter grade based on a numerical
+// // ========== Question = #1 — Determine If a Number Is Positive =============
+// //Write a program that determines whether a given number is positive or not.
 
 
-{
-   let number = 73;
-   let grade; 
-   if(number >= 90){
-      console.log('A')
-   }else if(number >= 80){
-      console.log('B')
-   }else if(number >= 70){
-   console.log('C')
-   }else{
-      console.log('D')
-   }
-}
+// {
+//    let a = 5;
+//    if( a > 4){
+//       console.log(" This Number is positive")
+//    }else {
+//       console.log("This number is not positive")
+//    }
+// }
 
-// ========== Question =  #5 - Ticket Pricing ============
-//Write a program that calculates the ticket price based on age.
+// // ========== Question =  #2 — Check Even or Odd =============
+// // Write a program that checks if a number is even or odd.
 
-let age = 22;
-let ticketprice;
+// {
+// let number = 9;
+// if ( number % 2 ===0){
+//    console.log('this is even number')
+// }
+// else {
+//    console.log('THis is odd number')
+// }
+// }
 
-if(age < 15){
-  console.log(`ticketprice = ${10}`)
-}else if(age >= 15 && age < 20){
-   console.log(`ticketprice = ${15}`)
-}else if(age >= 20 && age < 25){
-   console.log(`ticketprice = ${20}`)
-}
-else{
-   console.log(`ticketprice = ${30}`)
-}
 
-   // ========== Question =  #6 - Determine Leap Year ============
-   //Write a program that determines if a year is a leap year.
+// // ========== Question =   #3 — Determine the Greater Number =============
+// //Write a program to determine the greater of two numbers.
 
-   let year = 2024;
+// {
+//    let x = 13;
+//    let y = 17;
+//    if(x > y){
+//       console.log('x is greater')
+//    }else{
+//       console.log('y is greater')
+//    }
+// }
+
+// // ========== Question =    #4 — Grade Calculator ============
+// //Write a program that assigns a letter grade based on a numerical
+
+
+// {
+//    let number = 73;
+//    let grade; 
+//    if(number >= 90){
+//       console.log('A')
+//    }else if(number >= 80){
+//       console.log('B')
+//    }else if(number >= 70){
+//    console.log('C')
+//    }else{
+//       console.log('D')
+//    }
+// }
+
+// // ========== Question =  #5 - Ticket Pricing ============
+// //Write a program that calculates the ticket price based on age.
+
+// let age = 22;
+// let ticketprice;
+
+// if(age < 15){
+//   console.log(`ticketprice = ${10}`)
+// }else if(age >= 15 && age < 20){
+//    console.log(`ticketprice = ${15}`)
+// }else if(age >= 20 && age < 25){
+//    console.log(`ticketprice = ${20}`)
+// }
+// else{
+//    console.log(`ticketprice = ${30}`)
+// }
+
+//    // ========== Question =  #6 - Determine Leap Year ============
+//    //Write a program that determines if a year is a leap year.
+
+//    let year = 2024;
    
-   if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
-      console.log(year , 'is a leap year')
-   }else{
-      console.log(year, 'is not a leap year')
-   }
+//    if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
+//       console.log(year , 'is a leap year')
+//    }else{
+//       console.log(year, 'is not a leap year')
+//    }
 
-  //============ Question = #7 — Shopping Discount =============
-  //Write a program that calculates a discount based on the purchase amount.
-
-
-  let purchaseamount = 150;
-  let discount;
-
-  if(purchaseamount >= 120){
-   console.log(`discount = ${20} %`)
-  }else if(purchaseamount >= 80){
-   console.log(`discount = ${10} %`)
-  }else{
-   console.log(`discount = ${0} %`)
-  }
-
-    //============ Question = #8 — Time of Day Greeting =============
-    // Write a program that greets the user based on the time of day.
-
-    {
-    let time = new Date();
-    let hour = time.getHours();
-    console.log(time)
-    let greeting;
-    if(hour < 12){
-      console.log("Good morning")
-    }else if(hour < 18 )
-    {
-      console.log("Good afternoon")
-    }
-    else{
-      console.log("Good evening")
-    }
-   }
+//   //============ Question = #7 — Shopping Discount =============
+//   //Write a program that calculates a discount based on the purchase amount.
 
 
-    //============ Question = #9 —  Number Guessing Game===========
-    // Write a simple number guessing game.
+//   let purchaseamount = 150;
+//   let discount;
 
-    {
-      let number = 9;
-      let guess = 8;
+//   if(purchaseamount >= 120){
+//    console.log(`discount = ${20} %`)
+//   }else if(purchaseamount >= 80){
+//    console.log(`discount = ${10} %`)
+//   }else{
+//    console.log(`discount = ${0} %`)
+//   }
 
-      if(guess === number){
-         console.log("Congratulations! You guessed the correct number.")
-      }else if(guess < number){
-         console.log("Try a higher number.")
-      }
-      else{
-         console.log("Try a lower number.")
-      }
-    }
+//     //============ Question = #8 — Time of Day Greeting =============
+//     // Write a program that greets the user based on the time of day.
+
+//     {
+//     let time = new Date();
+//     let hour = time.getHours();
+//     console.log(time)
+//     let greeting;
+//     if(hour < 12){
+//       console.log("Good morning")
+//     }else if(hour < 18 )
+//     {
+//       console.log("Good afternoon")
+//     }
+//     else{
+//       console.log("Good evening")
+//     }
+//    }
+
+
+//     //============ Question = #9 —  Number Guessing Game===========
+//     // Write a simple number guessing game.
+
+//     {
+//       let number = 9;
+//       let guess = 8;
+
+//       if(guess === number){
+//          console.log("Congratulations! You guessed the correct number.")
+//       }else if(guess < number){
+//          console.log("Try a higher number.")
+//       }
+//       else{
+//          console.log("Try a lower number.")
+//       }
+//     }
+
+// // {
+// //     const min = 1;
+// // const max = 6;
+// // console.log(Math.floor(Math.random() * (max - min +1) + min));
+// // }
+
+
+// {
+// function myFunction(a , b){
+//    x = a*b;
+//    return a * b 
+// }
+// let val = myFunction(33,63)
+// console.log(x)
+// }
+
+
+// const sum = (x, y)=>{
+//    z = x + y;
+//    return z;
+//  }
+//  let val = sum(43,78)
+//  console.log(z)
+
+
+
+// //  ========== arrays ============
+// {
+// let marks = [47, 64, 93, 47, 39, 83]
+// console.log(marks)
+// console.log(marks.length)//property
+// }
+
+// {
+//    let marks = [47, 64, 93, 47, 39, 83]
+//    marks [0]=66 // change only array marks 
+//    console.log(marks)
+//    console.log(marks.length)//property
+//    }
+
+//    {
+//       let heros =['Thor', 'Spiderman', 'Betman', 'Hulk', 'Loki']
+//       // using a for loop
+//       for(let i = 0; i < heros.length; i++){
+//          console.log(heros[i])
+//       }
+//    }
+// {
+//       let cities =['Goa', 'Pune', 'Haryana', 'Delhi', 'Mumbai']
+//       // using an for of loop
+//       for(let city of cities){
+//          console.log(city.toUpperCase())
+//       }
+//    }
+
+//    // ========practice Array ===========
+// {
+//    // using for of
+//    let marks = [85, 97, 44, 37, 76, 60]
+//   let sum = 0;
+//   for (let val of marks){
+//    sum = sum + val
+//   }
+//   let avg = sum / marks.length
+//   console.log(avg)
+// }
+// {
+//    // using for loop
+//    let marks = [85, 97, 44, 37, 76, 60];
+//    let sum = 0;
+//    let count = marks.length;
+//    for(let i = 0; i < count; i++){
+//     sum += marks[i]
+//    }
+//    let avg = sum / count;
+//    console.log(avg)
+
+// }
+   
+// {
+//    // Smallest value p
+//    let marks = [85, 97, 44, 22, 37, 76, 60, 25];
+
+// let smallestNumber = marks[0]; //-> 85
+
+// for(let i=0; i<marks.length; i++){
+//  if(smallestNumber > marks[i])
+//    smallestNumber = marks[i]
+// }
+// console.log('smallest value:',smallestNumber);
+// }
+
 
 {
-    const min = 1;
-const max = 6;
-console.log(Math.floor(Math.random() * (max - min +1) + min));
+   let numbers = [95, 55, 34, 3, 30, 30, 48, 27]
+   let smallNUM = numbers[0];
+
+   for (let i=0; i<numbers.length-1; i++){
+      if(smallNUM < numbers[i]){
+         smallNUM = smallNUM;
+      } else{
+         smallNUM = numbers[i];
+      }
+   }
+   console.log(smallNUM);
 }
