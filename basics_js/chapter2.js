813a283f62a5fe8c70db74d9f06f009cf0acf8a8
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-08-08 14:07:48 +0000

    chapter 2

---
+++ basics_js/chapter2.js
@@ -0,0 +1,156 @@
+//  ========= chapter 2 ======= 
+
+//  ==== airthmetic operators ====
+ 
+// let a = 58;
+// let b = 4;
+// console.log('a + b = ',a+b); // adding operator
+// console.log('a - b = ',a-b); // subtraction
+// console.log('a * b = ',a*b); // multiplication
+// console.log('a / b = ',a/b); // division
+// console.log('a ** b = ',a**b); // Exponentiation
+// console.log('a % b = ',a%b); // modulus
+// console.log('++a = ',++a); // increment
+// console.log('--a = ',--a); // decrement
+
+
+// ==== assignment operators ====
+//. === += , -= , *= , /= , %= , **= ===
+
+// let assignment = 7;
+// assignment **= 5;
+// console.log(assignment)
+
+
+// ==== Comparison operators ====
+
+// let anky = 87;
+// let viney = 76;
+// console.log('anky == viney =',anky == viney) // equal to
+// console.log('anky != viney =',anky!= viney) // not equal to
+// console.log('anky === viney =',anky === viney) // equal value and equal type
+// console.log('anky !== viney =',anky !== viney) // not equal value or not equal type
+// console.log('anky < viney =',anky < viney) // less then 
+// console.log('anky > viney =',anky > viney) // greater then
+// console.log('anky >= viney =',anky >= viney) // greater than or equal to
+// console.log('anky <= viney =',anky <= viney) // less than or equal to
+
+// ==== Logical operators ====
+
+// let x = 39;
+// let y = 49;
+
+// console.log(x>y && x==39);
+// console.log(x<y && x==39);
+// console.log(x>y || x==39);
+// console.log(x<y || x==39);
+// console.log(!(x>y));
+// console.log(!(x<y));
+
+
+// ================= Conditional ==================
+// === if statement ===
+// === if else statement ===
+// === if else if else statement ===
+
+// let a = prompt("what is your age");
+// a = Number.parseInt(a) // Converting the string to number 
+//  console.log(typeof a);
+
+
+// if (a > 17) {
+//     alert("This is a valid age");  // if statement
+//   } else {
+//     alert("This is an invalid age"); // if else statement
+//   }
+//   console.log(a);
+
+// let x = prompt("what is your age");
+
+// // == if else if else statement ==
+
+// if (x < 0) {
+//   console.log("This is a invalid age");
+// } else if (x < 18) {
+//   console.log("You are a kid");
+// } else {
+//   console.log("You are a adult");
+// }
+
+
+// ==== switch ====
+
+// let color = 'red';
+// switch (color){
+//   case 'pink':
+//   console.log("color is Pink")
+//   break
+//   case 'yellow':
+//   console.log("color is Yellow")
+//   break
+//   case 'grey':
+//   console.log("color is Grey")
+//   break
+//   case 'red':
+//   console.log("color is red")
+//   break
+//   default: 
+//   console.log("I have no idea hahahahha")
+// }
+
+// === shortcut method === ternory operator
+
+// let age = 18
+// console.log(age < 18? "not drive" : "drive")
+
+// ======================= practice ======================
+
+// let age = prompt("what is your age")
+
+// if(age>=10 && age<=20){
+//   console.log('Your age lies between 10 and 20')
+// }else{
+//     console.log('Your age dosent lies between 10 and 20')
+// }
+
+// let age = 16;
+// if(age>=10 && age<=20){
+//   console.log('Your age lies between 10 and 20')
+// }else{
+//     console.log('Your age dosent lies between 10 and 20')
+// }
+
+// let age = prompt("what is your age");
+
+// switch (age) {
+//   case "12":
+//     console.log("Your age is 12");
+//     break;
+//   case "13":
+//     console.log("Your age is 13");
+//     break;
+//   case "14":
+//     console.log("Your age is 14");
+//     break;
+//   case "15":
+//     console.log("Your age is 15");
+//     break;
+//   default:
+//     console.log("Your age is not special");
+// }
+
+let num = prompt("what is your age");
+
+if (num % 2 == 0 && num % 3 == 0) {
+  console.log("your number is divisible by 2 & 3");
+} else {
+  console.log("your number is not divisible by 2 & 3");
+}
+
+
+
+
+
+
+
+
