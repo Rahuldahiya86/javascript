77895f2d15dd48c53790b69fc27d6517f59fe586
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-08-08 09:47:25 +0000

    Chapter 1

---
+++ basics_js/test2.js
@@ -0,0 +1,132 @@
+// == Code with harry ==
+
+// ======== Variable =========
+// // Javascript (case sensetive hoti hai)
+
+// {
+
+// let harry = "Hello World!"; // Variable start letters
+// console.log(harry)
+
+// let _harry = "Hello World!"; // Variable start underscores
+// console.log(_harry)
+
+// let $harry = "Hello World"; // Variable start $ Sign
+// console.log($harry)
+
+// let harry2 = "Hello World!";  // Variable digits allowed
+// console.log(harry2)
+
+// let 5harry = "Hello World!"; // error Variable can not start number
+// console.log(5harry)      
+
+// }
+
+// { 
+
+    // ====== Var , Let or Const =====
+
+    // === Var ===
+    // var a = 10;
+    // var a = 30; // Var 1 Global scope hai or hmm redeclare & update kr skte hai
+    // var b = 34;
+    // var d = 49;
+    // {
+    //     var b = 37;
+    // console.log(b)
+    // }
+    // console.log(a)
+
+    // === let ===
+
+    // let a = "Hello";
+    // let a = 54; // Redeclare nhi kr skte Error show hoga 
+
+    // {
+    //     let b = 50;
+    //     b = 94; // Update kr skte hai or ye block scope hota hai {}
+    //     console.log(b)
+    // }
+
+    // === Const ===
+
+    // {
+    //     const harry = 34; // Const block scope hota hai na update kr skte or na redeclare kr skte
+    //         //   harry = 49; // error show hoga redeclare ya update kre gy toh
+    //           console.log(harry)
+
+    // }
+
+// }
+
+// {
+
+    //  ======= Datatypes ======
+
+//     //  7 Primitive datatype 
+//    // NN SS BB U
+//    // 1 = Number = 123
+//    // 2 = null 
+//    // 3 = 'String' 
+//    // 4 = Symbol ('symbol')
+//    // 5 = Boolean = True / false
+//    // 6 = BigInt (234567)
+//    // 7 = undefined
+
+//    // 1 non-Primitive datatype 
+//    // 1 = Object
+
+//   ===== Example primitive =====
+// {
+//     let a = 46;
+//     let b = null;
+//     let c = "hello"
+//     let d = Symbol ('Hello World!')
+//     let e = true // false
+//     let f = BigInt (345)
+//     let g ;
+//     console.log(a, b, c, d, e, f, g)
+// }
+
+//   ===== Example non-primitive ===== // Object Key Value pair hote hai 
+// {
+//    let obj={
+//       'Anuj' : 45,
+//       'Vicky' : null,
+//       'Sahil' : "Hello Sahil",
+//       'Ritik' : Symbol ('Hello Ritik'),
+//       'Vinay' : false
+//    }
+//    console.log(obj)
+//    console.log(obj['Sahil'])
+//    console.log(typeof 'Vinay')
+// }
+
+// }
+
+// ====== practice chapter -1 ======
+
+// let a = 'Rahul';
+// let b = 48;
+// console.log(a + b)
+// console.log(typeof (a+b))
+
+
+// // can you change it to hold a number later ?
+// const a = {
+//    name: 'Rahul',
+//    marks: 40,
+//    isTeacher: false
+// }
+// a = 'Vicky' // error show hoga
+
+
+const a1 = {
+   name: 'Rahul',
+   marks: 40,
+   isTeacher: false
+}
+a1 ['friend'] = 'Vicky' // error show hoga
+console.log(a1)
+
+
