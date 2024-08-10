89646982f4a565b48f347badedcc215dcfc075a0
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-08-10 07:13:27 +0000

    arrays

---
+++ basics_js/chapter5.js
@@ -0,0 +1,44 @@
+// ====== arrays ======
+
+// let array =[39, 74, 38, 57, false, "Hello"]
+// array[1] = 21; // change the value of an array
+// array[6] = 87; // Adding a value of an array
+// console.log(array[0]) ;
+// console.log(array[1]) ;
+// console.log(array[2]) ;
+// console.log(array[3]) ;
+// console.log(array[4]) ;
+// console.log(array[5]) ;
+// console.log(array)
+// console.log("The length of ",array.length) // check length
+// console.log(typeof array)
+
+// for(let i = 0 ; i<array.length; i++){
+//     console.log(array[i])
+// }
+
+// === arrays methods ===
+
+let x = [49, 58, 87, 89, 65, "Hello"]
+// let y = x.toString() // convert to string
+// console.log(x)
+// console.log(y)
+// console.log(typeof y)
+// let z = x.join("_") // add something & new array return...............
+// console.log(x)
+// console.log(x,z)
+// console.log(typeof z)
+// let a = x.pop() // pop return the popped element
+// console.log(x)
+// console.log(x,a)
+let b = x.push("90") // push return the new array length.................
+console.log(x)
+console.log(x,b)
+// let c = x.shift() // remove an element from the start of the array
+// console.log(x)
+// console.log(x,c)
+// let d = x.unshift(78) // add an element from the start of the array 
+// console.log(x)
+// console.log(x,d)
+
+
